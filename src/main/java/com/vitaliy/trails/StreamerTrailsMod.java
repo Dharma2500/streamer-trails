eddce821ef7d7d4be5a15e6fdb441672a00c0850
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-01 13:09:55 +0500

    Update StreamerTrailsMod.java

--- src/main/java/com/vitaliy/trails/StreamerTrailsMod.java
+++ src/main/java/com/vitaliy/trails/StreamerTrailsMod.java
@@ -1,19 +1,12 @@
 package com.vitaliy.trails;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
-NetworkHandler.register();
+
 @Mod("streamertrails")
 public class StreamerTrailsMod {
 
     public StreamerTrailsMod() {
-NetworkHandler.register();
-        ModLoadingContext.get().registerConfig(
-                ModConfig.Type.CLIENT,
-                ModConfigHolder.SPEC
-        );
+        // Пусто. Никаких NetworkHandler тут больше нет.
     }
+
 }
-
-
