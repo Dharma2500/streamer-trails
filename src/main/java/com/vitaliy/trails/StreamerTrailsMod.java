d0216786692ae45c4f37335cb8347eeccbabff62
Dharma2500 <52962716+Dharma2500@users.noreply.github.com>
2026-03-01 06:14:55 +0500

    Update StreamerTrailsMod.java

--- src/main/java/com/vitaliy/trails/StreamerTrailsMod.java
+++ src/main/java/com/vitaliy/trails/StreamerTrailsMod.java
@@ -1,15 +1,17 @@
 package com.vitaliy.trails;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
 
-ModLoadingContext.get().registerConfig(
-        net.minecraftforge.fml.config.ModConfig.Type.CLIENT,
-        StreamerTrails.ModConfig.SPEC
-);
 @Mod("streamertrails")
 public class StreamerTrailsMod {
 
-}
+    public StreamerTrailsMod() {
 
+        ModLoadingContext.get().registerConfig(
+                ModConfig.Type.CLIENT,
+                ModConfigHolder.SPEC
+        );
+    }
+}
